ede8d57cfca10109093007f0a6b3341d9e3dacf1
Sine22 <saizayyartun10@gmail.com>
2025-12-02 22:32:02 +0700

    change sine-> people

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type Simple struct {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    simple := Simple{"Hello", "Sine", r.Host}
+    simple := Simple{"Hello", "People", r.Host}
 
     jsonOutput, _ := json.Marshal(simple)
 
