368373f6db03ec395e722a368f365cad86299153
Sai Zayar Tun <151984847+Sine22@users.noreply.github.com>
2025-12-15 11:35:37 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1" # change if needed
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0df7a207adb9748c7" # Ubuntu 22.04 (Singapore)
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
